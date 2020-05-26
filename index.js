e8bf01d6721919c413345dbe0de6bff96132fa28
Henri <henri.sundquist@rokki.net>
2020-05-26 12:19:06 +0300

    step3

--- index.js
+++ index.js
@@ -32,7 +32,12 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(p =>  p.id === id)
-    response.json(person)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
