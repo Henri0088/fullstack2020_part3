85f04fe255f01b91aa52664cebf68575307c96fc
Henri <henri.sundquist@rokki.net>
2020-06-04 16:38:51 +0300

    added lint and fixed lint issues in index.js and contact.js

--- index.js
+++ index.js
@@ -12,93 +12,93 @@ morgan.token('post-data', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(result => {
-        response.json(result)
-    })
+	Contact.find({}).then(result => {
+		response.json(result)
+	})
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const person = Object.assign({}, request.body)
-    
-    const newPerson = new Contact({
-        name: person.name,
-        number: person.number,
-    })
+	const person = Object.assign({}, request.body)
 
-    newPerson.save().then(savedPerson => {
-        console.log(`Saved ${savedPerson} successfully`)
-        Contact.find({}).then(result => {
-            response.json(result)
-        })
-    })
-    .catch(error => next(error))
-    
+	const newPerson = new Contact({
+		name: person.name,
+		number: person.number,
+	})
+
+	newPerson.save().then(savedPerson => {
+		console.log(`Saved ${savedPerson} successfully`)
+		Contact.find({}).then(result => {
+			response.json(result)
+		})
+	})
+		.catch(error => next(error))
+
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    
-    Contact.findById(id)
-        .then(result => {
-            response.json(result)
-        })
-        .catch(error => next(error))
+app.get('/api/persons/:id', (request, response, next) => {
+	const id = request.params.id
+
+	Contact.findById(id)
+		.then(result => {
+			response.json(result)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const newInfo = request.body
+	const newInfo = request.body
 
-    const contact = {
-        name: newInfo.name,
-        number: newInfo.number
-    }
+	const contact = {
+		name: newInfo.name,
+		number: newInfo.number
+	}
 
-    Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
-        .then(updatedContact => {
-            response.json(updatedContact)
-        })
-        .catch(error => next(error))
+	Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+		.then(updatedContact => {
+			response.json(updatedContact)
+		})
+		.catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Contact.findByIdAndRemove(id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+	const id = request.params.id
+	Contact.findByIdAndRemove(id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    let date = new Date()
-    let day = date.toDateString()
-    let time = date.toTimeString()
-    
-    Contact.countDocuments({})
-        .then(count => {
-            const page = (`<p>Phonebook has info for ${count} people <br> ${day} ${time} </p>`)
-        response.send(page)
-        })
+	let date = new Date()
+	let day = date.toDateString()
+	let time = date.toTimeString()
+
+	Contact.countDocuments({})
+		.then(count => {
+			const page = (`<p>Phonebook has info for ${count} people <br> ${day} ${time} </p>`)
+			response.send(page)
+		})
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'})
+	res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    if (error.name === "CastError") {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (error.name === "ValidationError") {
-        return res.status(400).json({ error: error.message })
-    }
-    next(error)
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return res.status(400).json({ error: error.message })
+	}
+	next(error)
 }
 
 app.use(errorHandler)
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+	console.log(`Server running on port ${port}`)
 })
--- models/contact.js
+++ models/contact.js
@@ -6,31 +6,31 @@ mongoose.set('useCreateIndex', true)
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB', error.message)
-    })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(result => {
+		console.log('connected to MongoDB')
+	})
+	.catch(error => {
+		console.log('error connecting to MongoDB', error.message)
+	})
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String, unique: true, minlength: 3, required: true
-    },
-    number: {
-        type: String, minlength: 8, required: true
-    }
+	name: {
+		type: String, unique: true, minlength: 3, required: true
+	},
+	number: {
+		type: String, minlength: 8, required: true
+	}
 })
 
 contactSchema.plugin(uniqueVal)
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Contact', contactSchema)
